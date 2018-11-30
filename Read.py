d258f6abba5df83ca353a5b9a073481b309a8aa5
Shri <45428369+ShrikantNande@users.noreply.github.com>
2018-11-30 08:27:28 +0530

    Update Read.py

--- Read.py
+++ Read.py
@@ -1,16 +1,16 @@
 #ShrikantNande
 #Read Different types of File
-def Read_file():#Read the files 
+def Read_file():#Read the Files 
     #split Filename
-    file = "housing.csv" #file name
+    file = "housing.csv" #File name
     filetype = file.split(".")[1] #Split filename and take extension
-    print('File Type:',filetype) #print filename
+    print('File Type:',filetype) #Print filename
     #File Path 
     import os #Import library to acccess os
     import os.path #Import library to access path
     os.chdir( "/home/Desktop/ZEUS") #Path for the file directory
     #Read File
-    def file_data_read(filetype):#read the file with corresponding with extension
+    def file_data_read(filetype):#Read the file with corresponding with extension
         if filetype=='csv': 
             data=pd.read_csv(file)#Read CSV file.
         elif filetype=='xls':
@@ -20,8 +20,8 @@ def Read_file():#Read the files
         elif filetype=='json':
             data=pd.read_json(file)#Read json file.
         else:
-            print('file not found') #print the file type is not found
-        return data #write data into structured format  
-    data=file_data_read(filetype)#read the function file type read
+            print('file not found') #Print the file type is not found
+        return data #Write data into structured format  
+    data=file_data_read(filetype)#Read the function file type read
     print(data.head(5))
-Read_file()#call the function
+Read_file()#Call the function
