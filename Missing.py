efe599db765451c19ac85b062dd7b781100386c0
Shri <45428369+ShrikantNande@users.noreply.github.com>
2018-11-30 08:50:36 +0530

    Update Missing.py

--- Missing.py
+++ Missing.py
@@ -3,7 +3,7 @@
 missing_value=data.isnull().sum()#calculate total missing values in columns
 print(missing_value)#print missing with there column names
 
-#Visualization of missing value
+#Visualization 
 import seaborn as sns #visualization of missing values
 sns.heatmap(data.isnull(), yticklabels=False, cmap='viridis') #plot coloured heatmap for missing values
 data=data.dropna() #Drop The Missing Value Rows
