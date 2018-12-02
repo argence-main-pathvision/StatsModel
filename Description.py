8ecfffa9c1816137c45a289eef7b530580c01f64
Shri <45428369+ShrikantNande@users.noreply.github.com>
2018-12-02 16:53:07 +0530

    Update Description.py

--- Description.py
+++ Description.py
@@ -1,31 +1,32 @@
-#Generate discriptive Staistics:Mean Standard_Deviation Range Count
-print(data.describe())
+#Shrikant Nande
+#Describe Mean, Median for numarical values and Mode for categorical values.
 
+print(data.describe())#Generate discriptive Staistics:Mean Standard_Deviation Range Count
 
 #mean_median(dataframes)
-data_mean= data.mean()
-Data_mean = pd.DataFrame(data_mean)
+data_mean= data.mean()#Describels the mean of all variables
+Data_mean = pd.DataFrame(data_mean)#Create a dataframe 
 
-data_median=data.median()
-Data_median=pd.DataFrame(data_median)
+data_median=data.median()#Describes median of all variables 
+Data_median=pd.DataFrame(data_median)#Create a dataframe 
 
 #print mean and median for numarical values
-for i in num_cols:
-    m=Data_mean.loc[i].tolist()
-    print("Mean for {0} is {1}.".format(i, m))
-    n=Data_median.loc[i].tolist()
-    print("Median for {0} is {1}.".format(i, n))    
+for i in numarical_columns:#For all numarical values
+    m=Data_mean.loc[i].tolist()#Create list for 'i'th variable mean
+    print("Mean for {0} is {1}.".format(i, m))#Print mean
+    n=Data_median.loc[i].tolist()#Create list for 'i'th variable median
+    print("Median for {0} is {1}.".format(i, n))#Print Median    
     
 
 #DataFrame for modes    
-data_Mode=data.mode()
-data_Mode=data_Mode.T
-Data_Mode=pd.DataFrame(data_Mode)
-Data_Mode=Data_Mode.iloc[:,0:1]
+data_Mode=data.mode()#Describels the mode of all variables 
+data_Mode=data_Mode.T#Dataframe is into the vartical form so take transpose 
+Data_Mode=pd.DataFrame(data_Mode)#Create a dataframe 
+Data_Mode=Data_Mode.iloc[:,0:1]#Take first mode 
 #print mode for all the dataset variables
-for i in cat_cols:
-    z=Data_Mode.loc[i].tolist()
-    print("Mode for {0} is {1}.".format(i, z))  
+for i in categiorical_columns:
+    z=Data_Mode.loc[i].tolist()#Take a value which corresponds to columns name
+    print("Mode for {0} is {1}.".format(i, z))#Print Mode for categorical variables
 
 
 
