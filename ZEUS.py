5d2673ac2c2a7796ba8e9d501a98a0d96a800e67
Shri <45428369+ShrikantNande@users.noreply.github.com>
2018-12-02 17:25:03 +0530

    Update ZEUS.py

--- ZEUS.py
+++ ZEUS.py
@@ -1,23 +1,19 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
+#Read Different types of File
+import pandas as pd #import library read file, 
+import matplotlib.pyplot as plt #import library to visualization
+from pandas.plotting import scatter_matrix #import library to plot scatter plot
 
 #split Filename
-file = "housing.csv"
-filetype = file.split(".")[1]
-print('File Type:',filetype)
-
-
+file = "housing.csv" #file name
+filetype = file.split(".")[1] #Split filename and take extension
+print('File Type:',filetype) #print filename
 #File Path 
-import os
-import os.path
-os.chdir( "/home/Desktop/ZEUS")#Path for the file directory
-
-
+import os #Import library to acccess os
+import os.path #Import library to access path
+os.chdir( "/home/shri/Desktop/ZEUS") #Path for the file directory
 #Read File
-def read_file(filetype):
-    if filetype=='csv':
+def file_data_read(filetype):#read the file with corresponding with extension
+    if filetype=='csv': 
         data=pd.read_csv(file)#Read CSV file.
     elif filetype=='xls':
         data=pd.read_excel(file)#Read excel file.
@@ -26,40 +22,29 @@ def read_file(filetype):
     elif filetype=='json':
         data=pd.read_json(file)#Read json file.
     else:
-        print('file not found') 
-    return data
-
-
-data=read_file(filetype)
-print(data.head(5))
+        print('file not found') #print the file type is not found
+    return data #write data into structured format  
+data=file_data_read(filetype)#read the function file type read
+print(data.head(2))#print data
 
 #Missing Values
-missing_value=data.isnull().sum()
-print('Total missing values in dataset:')
-print(missing_value)
+missing_value=data.isnull().sum()#calculate total missing values in columns
+print(missing_value)#print missing with there column names
 
-##Visualization of missing value
-import seaborn as sns
-sns.heatmap(data.isnull(), yticklabels=False, cmap='viridis')
-plt.show()
-
-#Drop The Missing Value Rows
-data=data.dropna()
+#Visualization 
+import seaborn as sns #visualization of missing values
+sns.heatmap(data.isnull(), yticklabels=False, cmap='viridis') #plot coloured heatmap for missing values
+plt.show()#Show the plot
+data=data.dropna() #Drop The Missing Value Rows
 
 #Find Catagorical Values
-cols = data.columns
-num_cols = data._get_numeric_data().columns#list of numarical variables
-#print('Numaric Data columns:',num_cols)
-cat_cols=list(set(cols) - set(num_cols))#List of categorical variables
-#print('Categorial Data Columns:',cat_cols)
-
-for i in cat_cols:
+columns = data.columns# total columns names can be extracted
+numarical_columns = data._get_numeric_data().columns#list of numarical variables
+categorical_columns=list(set(columns) - set(numarical_columns))#List of categorical variables
+for i in categorical_columns: #for all categorical columns convert type form object to categorical 
      data[i] = pd.Categorical(data[i])#Convert Object Type to Categorical.
 
-print('Info:',data.info())
-
-
-
+data.info()#print dataset info-shape,type,column name
 
 
 #How to check a variable is continuous or discrete?
@@ -71,92 +56,91 @@ print('Info:',data.info())
    #Values don't contain leading zeros: +.1
 
 #Conert Numarical to Categorical Type
-
-def Read_cat(data):
-    col_names=data.columns.tolist()
-    threshold=0
-    cat_var= []
-    for i in col_names:
-        if data[i].dtype=='int64':
+def Read_categorical(data):#read categorical values
+    column_names=data.columns.tolist() #list for categorical data can be created
+    threshold=0 #Initialuize the threshold
+    categorical_variable= [] #Initilize categorical Variable
+    for i in column_names: #for each column condition can be checked
+        if data[i].dtype=='int64':#Values are integers: +.7
             #print(i)
-            threshold = threshold + 0.7
+            threshold = threshold + 0.7#Values are integers: add +.7 in threshold
+            #--property not showing the exact values so eliminate this condition and minimize the threshold by 0.3
             #from scipy import stats
             #p=stats.shapiro(data[i])[1]
             #if p < 0.05:
-             #   threshold = threshold + 0.3
-            if len(set(data[i])) > 10:
-                threshold = threshold + 0.3
+            #   threshold = threshold + 0.3
+            
+            if len(set(data[i])) > 10:#Values not contain a relatively small number of 10 unique values 
+                threshold = threshold + 0.3 #Values not contain a relatively small number of 10 unique values: add +.3
             #print(f'Threshold for {i} is {threshold}')
-            if(threshold<=0.7):
-                cat_var.append(i)
-            threshold = 0
-    return cat_var
+            if(threshold<=0.7):#If threshold showing lesser value than 0.7 as we elimniate noramilty condition above is consider to bs #categorical
+                categorical_variable.append(i) #append list as that column name 
+            threshold = 0 
+    return categorical_variable #return categorical variable
 
-Numaric_to_Categorical = Read_cat(data)
-print('New Categorical variable:',Numaric_to_Categorical)
-#Convert Datatype name to categorical
-for i in Numaric_to_Categorical:
-    data[i] = pd.Categorical(data[i])
+Numaric_to_Categorical = Read_categorical(data) #store as a numaric values which are showing categorical type of behave 
+print(Numaric_to_Categorical)#Print Numarical to categorical variables
+
+for i in Numaric_to_Categorical:#Convert Datatype name to categorical
+    data[i] = pd.Categorical(data[i])#data is in categorical form as categorical <- object, categorical
 
 #Find Final Number of Catagorical Values
-num_cols = data._get_numeric_data().columns#list of numarical variables
-print('Numaric Data columns:',num_cols)
-cat_cols=list(set(cols) - set(num_cols))#List of categorical variables
-print('Categorial Data Columns:',cat_cols)
+numaric_columns = data._get_numeric_data().columns#list of numarical variables
+print('Numaric Data columns:',numaric_columns)#print Numarical data
+categorical_columns=list(set(columns) - set(numaric_columns))#List of categorical variables
+print('Categorial Data Columns:',categorical_columns)#print all categorical variables
 
 
-#Generate discriptive Staistics:Mean Standard_Deviation Range Count
-print(data.describe())
+#Describe Mean, Median for numarical values and Mode for categorical values.
+print(data.describe())#Generate discriptive Staistics:Mean Standard_Deviation Range Count
 
+#mean_median(dataframes)
+data_mean= data.mean()#Describels the mean of all variables
+Data_mean = pd.DataFrame(data_mean)#Create a dataframe 
 
-#Mean(dataframes)
-data_mean= data.mean()
-Data_mean = pd.DataFrame(data_mean)
-
-#Median(dataframes)
-data_median=data.median()
-Data_median=pd.DataFrame(data_median)
+data_median=data.median()#Describes median of all variables 
+Data_median=pd.DataFrame(data_median)#Create a dataframe 
 
 #print mean and median for numarical values
-for i in num_cols:
-    m=Data_mean.loc[i].tolist()
-    print("Mean for {0} is {1}.".format(i, m))
-    n=Data_median.loc[i].tolist()
-    print("Median for {0} is {1}.".format(i, n))    
+for i in numarical_columns:#For all numarical values
+    m=Data_mean.loc[i].tolist()#Create list for 'i'th variable mean
+    print("Mean for {0} is {1}.".format(i, m))#Print mean
+    n=Data_median.loc[i].tolist()#Create list for 'i'th variable median
+    print("Median for {0} is {1}.".format(i, n))#Print Median    
     
-
 #DataFrame for modes    
-data_Mode=data.mode()
-data_Mode=data_Mode.T
-Data_Mode=pd.DataFrame(data_Mode)
-Data_Mode=Data_Mode.iloc[:,0:1]
-#print mode for all the dataset variablesDData_Mode=Data_Mode.iloc[:,0:1]ata_Mode=Data_Mode.iloc[:,0:1]
-for i in cat_cols:
-    z=Data_Mode.loc[i].tolist()
-    print("Mode for {0} is {1}.".format(i, z))  
-
+data_Mode=data.mode()#Describels the mode of all variables 
+data_Mode=data_Mode.T#Dataframe is into the vartical form so take transpose 
+Data_Mode=pd.DataFrame(data_Mode)#Create a dataframe 
+Data_Mode=Data_Mode.iloc[:,0:1]#Take first mode 
+#print mode for all the dataset variables
+for i in categorical_columns:
+    z=Data_Mode.loc[i].tolist()#Take a value which corresponds to columns name
+    print("Mode for {0} is {1}.".format(i, z))#Print Mode for categorical variables
+    
 #Visulization
-#Box_plot:- Numarical Values
-for i in num_cols:
-    print('Variable_name:',i)
-    #Histogram Plot
+#Numarical Values-(Hist,Box,Density,)
+for i in numarical_columns:#
+    print('Variable_name:',i)#Print variable name
+    #--Histogram Plot
     data[i].hist()
-    plt.show()
+    plt.show()#show the histogram
     #Box Plot
     data[i].plot(kind='box', subplots=True, layout=(1,2), sharex=False, sharey=False)
-    plt.show()
+    plt.show()#show the Box
     #Density Plot:-Numarical Values
     data[i].plot.kde()
-    plt.show()
-    #Scatter Matrix Plot 
-    scatter_matrix(data, alpha=0.2, figsize=(16, 16), diagonal='kde')
-    plt.show()
+    plt.show()#show Density plot
+    
+#Scatter Matrix Plot 
+scatter_matrix(data, alpha=0.2, figsize=(16, 16), diagonal='kde')
+plt.show()#show the scatter plot
+
+#Scatter Matrix Plot 
+scatter_matrix(data, alpha=0.2, figsize=(16, 16), diagonal='kde')
+plt.show()#show the scatter plot
 #Bar_plot:-Categorical values
-for i in cat_cols:
-    print('Variable_name:',i)
-    data[i].value_counts().head(10).plot.bar()
-    plt.show()
-
-
-
-
+for i in categorical_columns:
+    print('Variable_name:',i)#Print variable name
+    data[i].value_counts().head(10).plot.bar()#Count and plot variables 
+    plt.show()#show 
