beab8ef9e686be50e249c547fac77aa3d5fe02a2
Shri <45428369+ShrikantNande@users.noreply.github.com>
2018-12-02 17:25:10 +0530

    Update Visulization.py

--- Visulization.py
+++ Visulization.py
@@ -1,24 +1,23 @@
 #ShrikantNande
 #Visulization
-#Box_plot:- Numarical Values
-for i in num_cols:
-    print('Variable_name:',i)
-    #Histogram Plot
+#Numarical Values-(Hist,Box,Density,)
+for i in numarical_columns:#
+    print('Variable_name:',i)#Print variable name
+    #--Histogram Plot
     data[i].hist()
-    plt.show()
+    plt.show()#show the histogram
     #Box Plot
     data[i].plot(kind='box', subplots=True, layout=(1,2), sharex=False, sharey=False)
-    plt.show()
+    plt.show()#show the Box
     #Density Plot:-Numarical Values
     data[i].plot.kde()
-    plt.show()
+    plt.show()#show Density plot
+    
 #Scatter Matrix Plot 
 scatter_matrix(data, alpha=0.2, figsize=(16, 16), diagonal='kde')
-plt.show()
+plt.show()#show the scatter plot
 #Bar_plot:-Categorical values
-for i in cat_cols:
-    print('Variable_name:',i)
-    data[i].value_counts().head(10).plot.bar()
-    plt.show()
-
-
+for i in categorical_columns:
+    print('Variable_name:',i)#Print variable name
+    data[i].value_counts().head(10).plot.bar()#Count and plot variables 
+    plt.show()#show 
